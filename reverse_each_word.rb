6fae30ca59f02286c7e5727a4aad1572cf7dc0f5
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-09 14:56:17 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,12 @@
 #def reverse_each_word(phrase)
  # new_string = ""
-  new_array = phrase.split
-  new_array.each { |x|
-    new_string << " #{x.reverse}"
-  }
-  new_string
+ # new_array = phrase.split
+ # new_array.each { |x|
+  #  new_string << " #{x.reverse}"
+  #}
+  #new_string
 #end
+
 phrase = "Hello there, and how are you?"
 def reverse_each_word(phrase)
   new_array = phrase.split
