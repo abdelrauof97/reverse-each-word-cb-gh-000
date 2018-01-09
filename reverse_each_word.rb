ea2c90a87bb6a2ac68e57bb65e7b6ae9baec5dda
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-09 14:52:12 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-def reverse_each_word(phrase)
+#def reverse_each_word(phrase)
   new_string = ""
   new_array = phrase.split
   new_array.each { |x|
     new_string << " #{x.reverse}"
   }
   new_string
-end
+#end
 
 def reverse_each_word(phrase)
   new_string = ""
